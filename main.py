61a2b4533d4120bfed850168543fa3ca4df3cea3
lasmavisnevska <94110629+lasmavisnevska@users.noreply.github.com>
2021-12-09 11:08:56 +0000

    Add 1. uzd

--- main.py
+++ main.py
@@ -5,3 +5,18 @@ print("2 - otro uzdevumu")
 print("3 - trešo uzdevumu")
 print("____________________")
 print("Tu esi izvēlējies uzdevuma nr.")
+
+#1. uzdevums
+cetri_skaitli = []
+for n in range(4):
+  abcd = int(input("Ievadi četrus skaitļus, katru atsevišķi"))
+  cetri_skaitli.append(abcd)
+reizinajums = cetri_skaitli[1] * cetri_skaitli[3]  
+print(reizinajums)
+
+
+#3.uzdevums
+
+tests = []
+for k in range(3):
+  
